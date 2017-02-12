e72afae7a89b5b942f6298f8ba81867994dcbe87
William Lee <masteratsoccer@gmail.com>
2017-02-11 21:42:30 -0500

    pushf or won

--- iSpot.py
+++ iSpot.py
@@ -73,43 +73,27 @@ if not ok or frame is None:
 w = frame.shape[1]
 h = frame.shape[0]
 
+################################################################################################
+#                      Create the average frame for temporal thresholding                      #
+################################################################################################
+
 ##Create the averaged frame
 #frame dimensions  (360, 640, 3)
 average = np.zeros_like(frame, dtype=np.float32);
-#for now, create temporal frame with 20 frames
 
+#for now, create temporal frame with 20 frames
 temporalCapture = cv2.VideoCapture(input_filename)
 ok, currentFrame = temporalCapture.read()
 for i in range(1, 20):
     frameToAdd = currentFrame.astype(np.float32)
     average = np.add(average, currentFrame)
-    print('calculating temporal frame ', i, 'th iteration', average)
+    okT, currentFrame = temporalCapture.read(currentFrame) 
+
+     #print('calculating temporal frame ', i, 'th iteration', average)
 average = average/20
-print('final temporal frame ', average)
+#print('final temporal frame ', average)
 
-
-
-#pdb.set_trace()
-
-# while 1:
-#     ok, frame = capture.read(frame) 
-
-#     # Bail if none.
-#     if not ok or frame is None:
-#         print('Aborting in temporal average calculation')
-#         break
-
-#     if writer:
-#         writer.write(frame)
-#     # Throw it up on the screen.
-#     cv2.imshow('Video', frame)
-
-
-#     # Delay for 5ms and get a key
-#     k = cv2.waitKey(5)
-#     # Check for ESC hit:
-#     if k % 0x100 == 27:
-#         break
+################################################################################################
 
 # Now set up a VideoWriter to output video. (dependent on w, h above)
 
@@ -120,37 +104,31 @@ fps = 30 #do we want to downgrade?
 
 #huh, so .mov should work
 fourcc, ext = (cv2.VideoWriter_fourcc('M', 'P', '4', 'V'), 'mov')
-
 filename = 'captured.'+ext
-
 writer = cv2.VideoWriter(filename, fourcc, fps, (w, h))
 if not writer:
     print('Error opening writer')
 else:
     print('Opened', filename, 'for output.')
     writer.write(frame)
-
 # Loop until movie is ended or user hits ESC:
 frameNumber=0;
-cv2.imshow('frame', frame)
+
+
+
+################################################################################################
+#                                    Place computations here                                   #
+################################################################################################
+
 while 1:
-
-    # Get the frame.
     frameNumber = frameNumber + 1 #increment frameNumber
+    print(frameNumber)
+    # Get the frame.
     ok, frame = capture.read(frame) 
-
     # Bail if none.
     if not ok or frame is None:
         print('Bad frame in video! Aborting!')
         break
-
-    ################################################################################################
-    #                     ve vill do ze computations ere! (modify the current frame)               #
-    ################################################################################################
-    #print('ve vill do ze computations ere! ', frameNumber)
-    #print('frame size', w, ' ', h, "frame dimensions ", frame.shape)
-
-
 
     #TODO: First, RGB threshold the hands out in this frame
         #we may need to do this with a mask? to preseve color in the other image
@@ -166,7 +144,20 @@ while 1:
         
 
     #TODO: Here, do temporal threholding to remove all except the bar (non-glove part of arms will likely stay too)
+    grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY).astype(np.float32)
+    grayAverage = cv2.cvtColor(average, cv2.COLOR_BGR2GRAY).astype(np.float32)
+    diffMatrix = cv2.absdiff(grayAverage, grayFrame).astype(np.float32)
 
+
+
+#    temporalThreshold = np.zeros_like(average)
+#    temporalThreshold= cv2.threshold(diffMatrix, 50, cv2.THRESH_BINARY)
+    
+    ##trippy af bruh
+    #temporalThreshold= cv2.threshold(diffMatrix, 0, 255, cv2.THRESH_BINARY)
+
+    something, temporalThreshold = cv2.threshold(diffMatrix, 15, 255, cv2.THRESH_BINARY)
+    print(diffMatrix)    
 
     #do temporal averaging on the first few frames of 125_5x_lf_lowres.mov
         #create comparison frame by averaging the first few of the movie
@@ -189,8 +180,14 @@ while 1:
     if writer:
         writer.write(frame)
     # Throw it up on the screen.
-    cv2.imshow('Video', frame)    
-    cv2.imshow('average', average.astype(np.uint8))
+    #cv2.imshow('Video', frame)    
+    #cv2.imshow('average', average.astype(np.uint8))
+    #cv2.imshow('diff matrix', diffMatrix.astype(np.uint8))
+    #cv2.imshow('Gray Average', grayAverage.astype(np.uint8))
+    cv2.imshow('Gray Frame', grayFrame.astype(np.uint8))
+    cv2.imshow('Temporal Threshold', temporalThreshold.astype(np.uint8))
+    cv2.imshow('absdiff', diffMatrix.astype(np.uint8))
+
 #    pdb.set_trace()
 
 
